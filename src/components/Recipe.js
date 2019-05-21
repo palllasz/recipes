fdf13d708aeee2174744a6351999e19dd14b0a18
Laszlo Pal <pall.laszlo@web.de>
2019-05-21 17:43:50 +0200

    first recipes

---
+++ src/components/Recipe.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+export default class Recipe extends Component {
+  render() {
+    const {
+      image_url,
+      title,
+      source_url,
+      publisher,
+      recipe_id
+    } = this.props.recipe;
+    const { handleDetails } = this.props;
+    return (
+      <React.Fragment>
+        <div className="col-10 mx-auto col-md-6 col-lg-4 my-3">
+          <div className="card">
+            <img
+              src={image_url}
+              className="img-card-top"
+              style={{ height: "14rem" }}
+              alt="recipe"
+            />
+            <div className="card-body text-capitalize text-slanted">
+              <h3>{title}</h3>
+              <h6 className="text-warning">provided by{publisher}</h6>
+            </div>
+            <div className="card-footer">
+              <button
+                type="button"
+                className="btn btn-primary text-capitalize"
+                onClick={handleDetails}
+              >
+                details
+              </button>
+              <a
+                href={source_url}
+                className="btn btn-success mx-2 text-capitalize"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Go to the source
+              </a>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
